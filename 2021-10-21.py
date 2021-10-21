824f47eb8f7982f6938c1dbe18c0189c807a4a75
祉语 <8689418+xiaozhiyuqwq@user.noreply.gitee.com>
2021-10-21 06:53:59 +0000

    awa

---
+++ 2021-10-21.py
@@ -0,0 +1,11 @@
+#输入
+x=int(input("在此输入任意自然数......"))
+if x==0 :
+    print("您输入的自然数",x,"不属于奇数，也不属于偶数。")
+elif x%2==0 :
+    print("您输入的自然数",x,"属于偶数。")
+else:
+    print("您输入的自然数",x,"属于奇数。")
+#by xiaozhiyuqwq
+#https://xiaozhiyuqwq.top
+#2021-10-21 生日快乐！
