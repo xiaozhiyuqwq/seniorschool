3bf75a2c022918b7f13731c46eab971e54c43a60
祉语 <8689418+xiaozhiyuqwq@user.noreply.gitee.com>
2021-10-30 12:59:13 +0000

    update 20211028/40_2.py.

--- 20211028/40_2.py
+++ 20211028/40_2.py
@@ -5,10 +5,9 @@ while True:
     if food in foodlist1 and food in foodlist2:
         print("好耶！食物",food,"是我们都想吃的！")
         break
-    elif food in foodlist1 and food not in foodlist2:
-        print("啊嗷！食物",food,"有一个人不想吃。")
-    elif food not in foodlist1 and food in foodlist2:
+    elif food in foodlist1 and food not in foodlist2 or food not in foodlist1 and food in foodlist2:
         print("啊嗷！食物",food,"有一个人不想吃。")
     else:
         print("啊嗷！食物",food,"我们都不想吃。")
 #by xiaozhiyuqwq
+#https://xiaozhiyuqwq.top
