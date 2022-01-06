7375038b00a6d2deaec5d70bfac25ddbf4d2558e
祉语 <2534107610@qq.com>
2022-01-06 05:56:15 +0000

    考试题目

---
+++ 考试题目/最大公约数.py
@@ -0,0 +1,12 @@
+﻿#请不要更改源程序的结构，删除原题里的①、②、③。填写正确的代码，使程序完善
+num1=int(input('请输入第一个正整数：'))
+num2=int(input('请输入第二个正整数：'))
+m=max(num1,num2)
+n=min(num1,num2)
+r=m % n
+while r!=0:
+    m=n
+    n=r
+    r=m % n
+print('这两个数的最大公约数为：',n)
+input("运行完毕，请按回车键退出...") 
